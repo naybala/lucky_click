c52de5283cbbcbea22615cf76a9cd1c62cb73f39
Nay Ba La <naybalaismylife.nbl@gmail.com>
2025-05-27 00:05:32 +0630

    update : next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-const nextI18NextConfig = require("./next-i18next.config");
+const { i18n } = require("./next-i18next.config");
 
 module.exports = {
-  i18n: nextI18NextConfig.i18n,
+  i18n,
   reactStrictMode: true,
 };
